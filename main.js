d5fcb267d394f13efa52706073ebadf5e9bf8c99
iErnesto <ernesto1991diaz@gmail.com>
2020-12-26 15:34:52 -0600

    Created Order

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//let mealsState = [] // variable para guardar toda la data de meals
+
 const stringToHTML = (s) => {
     const parser = new DOMParser()
     const doc = parser.parseFromString(s, 'text/html')
@@ -23,17 +25,64 @@ const renderItem = (item) => {
     return element
 }
 
+const renderOrder = (order, meals) => {
+
+
+    const meal = meals.find(meal => meal._id === order.meal_id)
+    const element = stringToHTML(`<li data-id="${order._id}"> ${meal.name} - ${order.user_id}</li>`)
+
+    return element
+
+}
+
 window.onload = () => {
-    fetch('http://localhost:3000/api/meals',)
+
+    const orderForm = document.getElementById('order')
+    orderForm.onsubmit = (e) => {
+        e.preventDefault()
+        const mealId = document.getElementById('meals-id')
+        const mealIdValue = mealId.value
+        if (!mealIdValue) {
+            alert('Debe seleccionar un plato')
+            return
+        }
+
+        const order = {
+            meal_id: mealIdValue,
+            user_id: 'Miguel'
+        }
+
+        fetch('http://localhost:3000/api/orders', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+
+            body: JSON.stringify(order)
+
+        })
+            .then(x => console.log(x))
+
+    }
+
+    fetch('http://localhost:3000/api/meals')
         .then(response => response.json())
         .then(data => {
 
+            const mealsState = data
             const mealslist = document.getElementById('meals-list')
             const submit = document.getElementById('submit')
             const listItems = data.map(renderItem)
             mealslist.removeChild(mealslist.firstElementChild)
             listItems.forEach(element => mealslist.appendChild(element))
             submit.removeAttribute('disabled')
-
+            fetch('http://localhost:3000/api/orders')
+                .then(response => response.json())
+                .then(ordersData => {
+                    const ordersList = document.getElementById('orders-list')
+                    const listOrders = ordersData.map(orderData => renderOrder(orderData, data))
+                    ordersList.removeChild(ordersList.firstElementChild)
+                    listOrders.forEach(element => ordersList.appendChild(element))
+                })
         })
 }
